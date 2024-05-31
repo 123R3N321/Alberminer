88a369e4761d19b329dc8ab0318e2ac3f9da2175
123R3N321 <jr5887@nyu.edu>
2024-05-31 13:22:05 -0400

    Add comments for easy review/collaberation.

--- albertMiner.py
+++ albertMiner.py
@@ -60,19 +60,6 @@ class SelectorManager:
         self.switch = False
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
@@ -86,10 +73,15 @@ import time
 import requests
 
 
-timeout = 3
-driver = None
-urlSet = set()
+timeout = 3 #not exactly needed at the moment
+driver = None#global driver param. Do not touch
+urlSet = set()#not exactly needed.
 
+'''
+encapsulation for shorter non repetitive code
+however, there is no delay in this function
+use with caution
+'''
 def interact(field, content = None):
     where = driver.find_element(By.CSS_SELECTOR, field)
     if isinstance(content, str):    #fill in a string
@@ -99,14 +91,26 @@ def interact(field, content = None):
 
 
 # CSS selector for the scrollable container
+'''
+shld prolly place this right above while loop inside run() function.
+scrollabl container selector.
+'''
 scrollable_container_selector = "div.panel__body:nth-child(3)"
 # step_height = 0  # Amount to scroll each time
 
-# Function to scroll the container by a step
+'''
+simple one-step scroll, 
+step_height to be controlled externally
+(preferrably dynamically if dynamic control is fast enough)
+'''
 def scroll_by_step(driver, selector, step_height = 80):
     container = driver.find_element(By.CSS_SELECTOR, selector)
     driver.execute_script("arguments[0].scrollTop += arguments[1];", container, step_height)
 
+'''
+somehow breaks the selenium functionality. 0.1 zoom out to tiny fonts, 2 zoom in to huge
+might be useful in the future
+'''
 def zoom_out(driver, zoom_level=0.5):
     driver.execute_script(f"document.body.style.zoom='{zoom_level}'")
 
@@ -129,9 +133,12 @@ def dynamicScrollAdjust(upperBound=300, lowerBound=600, itemTop=450, itemSize=10
         adjust = itemSize
     return adjust
 
-
+'''
+the function that does the mining
+'''
 def run(): #do not load image for better speed
     global driver
+    extraDelay = 10 #for extra slow steps
     # I manually put it inside usr, otherwise need to specify driver file path
     options = Options()
     options.add_argument('--blink-settings=imagesEnabled=false')  # disable loading image
@@ -144,47 +151,45 @@ def run(): #do not load image for better speed
         EC.presence_of_element_located((By.CSS_SELECTOR, "div.textBox:nth-child(2) > div:nth-child(3) > a:nth-child(1)"))
     )
     albertButton.click()
-    time.sleep(2)
-    select_element = WebDriverWait(driver, 10).until(
+    time.sleep(2)   #pretending I am a human
+    select_element = WebDriverWait(driver, extraDelay).until(   #slow step, extra wait
         EC.presence_of_element_located((By.ID, "crit-camp"))  # Adjust the selector as needed
     )
 
     dropdown = Select(select_element)
 
-    # Select the option with value "UY"
+    # Select the option for brooklyn campus.
     dropdown.select_by_value("WS@BRKLN")   #this is changed for dff campuses. I selected brooklyn here
 
     # Verify the selected option (optional)
     selected_option = dropdown.first_selected_option
     print(f"Selected option: {selected_option.text}")
 
-    startSearch = WebDriverWait(driver, 10).until(
+    startSearch = WebDriverWait(driver, extraDelay).until(  #slow step. Extra wait
         EC.presence_of_element_located((By.ID, "search-button"))  # Adjust the selector as needed
     )
     startSearch.click()
 
-
-
-
+    # Initialize selector manager. Can add more selectors if needed.
     selectorManager = SelectorManager(["div.result:nth-child(IND) > a:nth-child(1)",
                                        "div.result:nth-child(IND)"])
 
-    failCt = 0
-    successCt = 0
-    unknownCt = 0
-    breaker = 255
-    inPageDelay = 0.2
-    delayFactor = 5
-    step_height = 0  # Amount to scroll each time
-    correction = 50
+    failCt = 0      #tally failed mining. Includes cases with no data (there's nothing to mine)
+    successCt = 0   #tally successful data mining
+    unknownCt = 0   #tally encounters with undocumented css selector
+    breaker = 255   #infinite loop guard
+    inPageDelay = 0.2   #heuristic param based on internet speed and Albert server speed. Range 0.1-3
+    delayFactor = 5     #heuristic param as above. Range 1-10, dependent on inPageDelay linearly.
+    step_height = 0     # Initial amount to scroll each time. Not exactly necessary
+    correction = 50     #heuristic scrolling correction.
 
-    scanRange = 200
+    scanRange = 200     #how many classes we are mining at
 
     # zoom_out(driver,0.2)
     '''
     somehow, zoom function breaks the scraping
     '''
-    container = WebDriverWait(driver, 10).until(
+    container = WebDriverWait(driver, extraDelay).until(
         EC.presence_of_element_located((By.CSS_SELECTOR,scrollable_container_selector ))
     )
     upperBound = container.location['y']
@@ -196,16 +201,16 @@ def run(): #do not load image for better speed
         selectorManager.setTop()
         selectorManager.update(1)
 
-        for i in range(len(selectorManager.rawData)):
+        for i in range(len(selectorManager.rawData)):   #lazy code to function as a safe while loop
             curSelector = selectorManager.roll()
-            if "FAIL"==curSelector:
+            if "FAIL"==curSelector: #we need to add more selector to selectorManager if this happens
                 print("unknown selector at n= ",selectorManager.n)
                 unknownCt +=1
                 scroll_by_step(driver, scrollable_container_selector, correction)
 
                 break
 
-            try:
+            try:    #attempt to locate course button
                 checkOne = WebDriverWait(driver, inPageDelay).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, curSelector))
                 )
@@ -220,7 +225,8 @@ def run(): #do not load image for better speed
                 print("                                     adjusted scroll size to be: ", step_height)
                 # print("succeeded, with list: ",selectorManager.rawData, " more specifically, selector is: ",curSelector)
 
-                try:
+                try:    #when course button located successfully,
+                        #further attempt to mine meeting loc and time info
                     locInfo = WebDriverWait(driver, inPageDelay * delayFactor).until(
                         EC.presence_of_element_located((By.CSS_SELECTOR,".meet"))
                     )
@@ -230,183 +236,21 @@ def run(): #do not load image for better speed
                     scroll_by_step(driver, scrollable_container_selector, step_height)
                     break
 
-                except Exception as e:
+                except Exception as e:  #no meeting info found
                     failCt+=1
                     print(f"Exception of getting meeting info at n={selectorManager.n}, the class prolly does not meet!")
-
+                    # correction small step scroll, which is safer
                     scroll_by_step(driver, scrollable_container_selector, correction)
 
                 break
 
-            except Exception as e:
+            except Exception as e: #course button not found. Need selectorManager to switch selector now.
                 pass
                 # print("selector switch happened, now we have: ",selectorManager.rawData)
 
-
-
-
-    # n=0
-    # breaker = 1024
-    # while n<50 and breaker>0:
-    #     n+=1
-    #     breaker -= 1
-    #
-    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #     try:
-    #         checkOne = WebDriverWait(driver, 0.2).until(
-    #             EC.presence_of_element_located((By.CSS_SELECTOR, f"div.result:nth-child({str(n)}) > a:nth-child(1)"))
-    #         )
-    #         checkOne.click()
-    #     except Exception as e:
-    #         try:
-    #             checkOne = WebDriverWait(driver, 0.2).until(
-    #                 EC.presence_of_element_located(
-    #                     (By.CSS_SELECTOR, f"div.result:nth-child({str(n)})"))
-    #             )
-    #             checkOne.click()
-    #         except Exception as e:
-    #             print(f"Exception of clicking at n={n}")
-    #         try:
-    #             locInfo = WebDriverWait(driver, 5).until(
-    #                 EC.presence_of_element_located((By.CSS_SELECTOR,".meet"))
-    #             )
-    #             print(locInfo.text, "at index n: ",n)
-    #         except Exception as e:
-    #             print(f"Exception of getting meeting info at n={n}")
     print("script complete, failCt: ",failCt, " successCt: ",successCt, " unknownCt: ",unknownCt)
     time.sleep(3)
 
-
-
-
-
-
-
-    # Wait for the search button to be clickable
-#     search_button = WebDriverWait(driver, timeout).until(
-#         EC.element_to_be_clickable((By.CSS_SELECTOR, "#username"))
-#     )
-#     # Click the search button
-#     search_button.click()
-#     # Optionally, wait for the search results to load
-#     WebDriverWait(driver, timeout).until(
-#         lambda d: d.find_element(By.CSS_SELECTOR, ".collection__title") and targetProduct in d.find_element(By.CSS_SELECTOR,
-#                                                                                            ".collection__title").text
-#     )
-#
-# #step into each product card and mine them
-# '''
-# teargetProduct is just fixed to be "tea"
-# filter True means we don want any product with vendor "HOUSE BRAND" (meaning loose tea)
-# curPage simply tracks page number, not very important
-# '''
-# def mineSinglePage(targetProduct = "tea", filter = True,curPage = 1):
-#     global driver
-#     global urlSet
-#     oldCt = len(urlSet)
-#     n = 0  # paramatrize item navig
-#     breaker = 255
-#     delay = 0.2  # when internet slows down we use a higher delay factor
-#     while breaker > 0:
-#         breaker -= 1
-#         n += 1
-#         try:
-#
-#             vendor_info = WebDriverWait(driver, timeout).until(
-#                 EC.presence_of_element_located((By.CSS_SELECTOR,
-#                                                 "div.product-item:nth-child(" + str(
-#                                                     n) + ") > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)"))
-#             )  # change the first index for diff products
-#         except TimeoutException:
-#             print("all products from page ",curPage," are extracted. Proceeding to next page...")
-#             return
-#
-#
-#
-#         # only skip if we enabled filter and detect loose tea keyword: "HOUSE BRAND"
-#         if (not ("HOUSE BRAND" in vendor_info.text and filter)):
-#             # Find the link of the first product item
-#             product_link = WebDriverWait(driver, timeout).until(
-#                 EC.element_to_be_clickable((By.CSS_SELECTOR,
-#                                                 "div.product-item:nth-child(" + str(
-#                                                     n) + ") > div:nth-child(2) > div:nth-child(1) > a:nth-child(2)"))
-#             )
-#             # product_link.click()
-#             #try javascript solution
-#             driver.execute_script("arguments[0].click();", product_link)
-#             # Wait for some time to let the network requests complete
-#             time.sleep(delay)
-#
-#             # check for sold out button not disabled
-#             soldOutButton = WebDriverWait(driver, timeout).until(
-#                 EC.presence_of_element_located((By.CSS_SELECTOR, ".product-form__add-button"))
-#             )
-#
-#             # sold out, we skip
-#             if(not soldOutButton.get_attribute("disabled")):
-#
-#                 ##########successfully located product. Now extraction starts#################
-#                 website = "https://foodsofnations.com"
-#
-#                 successFlag = False
-#                 for request in driver.requests:
-#                     if request.response:
-#                         if request.path.endswith('.json') and request.method == 'GET' and "products" in request.path:
-#                             urlSet.add(website + request.path)
-#                             successFlag = True
-#                             # Stop iterating once the first JSON file is found
-#                             # break
-#                 if not successFlag:
-#                     print("No JSON file found on initial page load. Refreshing the page...")
-#                     driver.refresh()
-#                     time.sleep(2 * delay)  # delay to make sure webpage loads
-#
-#                     for request in driver.requests:
-#                         if request.response:
-#                             if request.path.endswith('.json') and request.method == 'GET' and "products" in request.path:
-#                                 print("Found JSON URL after refreshing:", "https://foodsofnations.com" + request.path)
-#
-#
-#                 if not successFlag:
-#                     print("No JSON file found on second attempt at url: ")
-#                     print(driver.current_url)
-#                 ############extraction of single item complete. Navigate back
-#             driver.back()
-#
-#             WebDriverWait(driver, timeout).until(
-#                 lambda d: d.find_element(By.CSS_SELECTOR, ".collection__title") and targetProduct in d.find_element(By.CSS_SELECTOR,
-#                                                                                            ".collection__title").text
-#             )
-#             # above is the while loop for single page
-#     print("total urls added from page ",curPage, ": ",len(urlSet)-oldCt)
-#     for each in urlSet:
-#         print(each)
-#
-# def mineAllPages(targetProduct = "tea", filter = True):
-#     global driver
-#     global urlSet
-#     breaker = 255
-#     pageCt = 0
-#     while breaker > 0:
-#         breaker -= 1
-#         pageCt += 1
-#         print("loop executed for page flip")
-#         mineSinglePage(targetProduct = targetProduct, filter = filter,curPage = pageCt)
-#         try:
-#             nextPage = WebDriverWait(driver, timeout).until(
-#                 EC.presence_of_element_located((By.CSS_SELECTOR,
-#                                                 ".pagination__next"))
-#             )
-#             nextPage.click()
-#             driver.get(driver.current_url)
-#             print("one entire loop for page flip complete")
-#
-#
-#
-#         except TimeoutException:
-#             print("All pages extracted. finishing at page ",pageCt)
-#             print("Total urls added: ", len(urlSet))
-#             break
 
 
 # Function to fetch and parse JSON from URL
